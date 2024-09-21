6e0619b335a73b513f76cff7b47726254a849fe5
Akash Rahman <akashrahman.me@gmail.com>
2024-09-21 08:53:46 +0600

    updated the chrome driver and fixed the issues

--- DEMO.py
+++ DEMO.py
@@ -1,1 +1,25 @@
-print("Hello, World!")
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+
+try:
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")  # Enable headless mode
+    firefox_options.add_argument("--window-size=1366,768")
+    firefox_options.add_argument("--no-sandbox")  # No sandbox mode
+    firefox_options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")  # Set custom user agent
+
+    # Automation-controlled workaround
+    firefox_options.set_preference("dom.webdriver.enabled", False)
+    firefox_options.set_preference('useAutomationExtension', False)
+
+    # Use the Service object for the GeckoDriver
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=firefox_options)
+
+    driver.get("https://www.google.com")
+    driver.save_screenshot('akashrahman_me.png')
+
+except Exception as e:
+    print(e)
+    pass
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
+from plyer import notification
 from lib.TimeoutRunner import TimeoutRunner
 from lib.OnlineStatusChecker import OnlineStatusChecker
 from utils.notify import notify
 import os
+
 
 load_dotenv()
 ACCOUNT_PATH = os.getenv('ACCOUNT_PATH')
@@ -21,6 +23,11 @@ def checker_func():
     return result
 
 if __name__ == '__main__':
+    notification.notify(
+        title="Inspect the online analysis",
+        message="We will send you a notification with the result within the next minute, ensuring you receive the update promptly."
+    )
+
     timer = TimeoutRunner()
     timer.run_with_timeout(checker_func, 60, alert_timeout)
 
